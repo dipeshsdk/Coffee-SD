d62f3398dd6abfe200c78514c73fb4bf15b25f68
dipeshsdk <sharmadipesh.sdk@gmail.com>
2024-06-09 11:18:00 +0530

    responsive changes

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});  //this section from line no 10 to 14 is write for nav section to stuck and apply bg color while site scroll to down
